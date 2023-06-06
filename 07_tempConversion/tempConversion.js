1f1791a4db761a08f9a2cc69d894ccd0a05c824f
Aditya Saran <135217588+adhi-26@users.noreply.github.com>
2023-06-06 20:54:04 +0530

    Fix 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(frnht) {
+    let cel = (frnht-32)*5/9;
+    cel = Math.round(10*cel)/10;
+    return cel;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cels) {
+  fnht = cels*9/5 + 32;
+  fnht = Math.round(10*fnht)/10;
+  return fnht;
 };
 
 // Do not edit below this line
