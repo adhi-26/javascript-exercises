d088801a1f34d2950e9b02dbb633e47140a206ca
Aditya Saran <135217588+adhi-26@users.noreply.github.com>
2023-06-06 18:56:26 +0530

    Fix 02_repeatedString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string,num) {
+    let copy = string;
+    if (num<0) {
+        return "ERROR";
+    }else if (num==0) {
+        return '';
+    }
+    for (i=1;i<num;i++) {
+        string = string.concat(copy);
+    }
+    return string;
 };
 
 // Do not edit below this line
